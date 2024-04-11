0966a6ad989d7569eabeb5471e640103fe724672
raphael.kieling <raphael.kieling-ext@ab-inbev.com>
2024-04-11 13:47:07 -0300

    adding another color and hop for F again

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
-return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    require('catppuccin').setup {}
-    vim.cmd 'colorscheme catppuccin'
-  end,
-}
 -- return {
---   'rose-pine/neovim',
---   name = 'rose-pine',
+--   'catppuccin/nvim',
+--   name = 'catppuccin',
+--   priority = 1000,
 --   config = function()
---     require('rose-pine').setup {
---       variant = 'auto',
---     }
---     vim.cmd 'colorscheme rose-pine'
+--     require('catppuccin').setup {}
+--     vim.cmd 'colorscheme catppuccin'
 --   end,
 -- }
+return {
+  'rose-pine/neovim',
+  name = 'rose-pine',
+  config = function()
+    require('rose-pine').setup {
+      variant = 'auto',
+    }
+    vim.cmd 'colorscheme rose-pine'
+  end,
+}
--- lua/custom/plugins/jump.lua
+++ lua/custom/plugins/jump.lua
@@ -3,7 +3,7 @@ return {
   branch = 'v2', -- optional but strongly recommended
   config = function()
     -- Hop configuration
-    vim.api.nvim_set_keymap('', '<Tab>', '<cmd>HopChar1MW<CR>', {})
+    vim.api.nvim_set_keymap('', 'f', '<cmd>HopChar1MW<CR>', {})
     -- vim.api.nvim_set_keymap('n', 't', '<cmd>HopPattern<CR>', { noremap = true })
     -- you can configure Hop the way you like here; see :h hop-config
     require('hop').setup { keys = 'etovxqpdygfblzhckisuran' }
